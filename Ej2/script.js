353d29fabfb9fc88fa3f4d96f5fcb0eeec46c4c2
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-19 21:59:27 -0300

    agregue un limite de imgs

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,15 +1,26 @@
 const fieldSet = document.querySelector("fieldset");
 const IMG_BASE = "https://wahooart.com/Art.nsf/O/8XXRVZ/$File/Claude-Monet-Pathway-in-Monet-s-Garden-at-Giverny.jpg";
+const MAX_IMGS = 3;
 
+let contImgs = 0;
 
 function agregarImg() {
-    fieldSet.innerHTML += `
+    
+    if (contImgs <= MAX_IMGS - 1) {
+        fieldSet.innerHTML += `
         <img src="${IMG_BASE}" alt="test">
     `;
+
+    contImgs = contImgs + 1;
+
+    }
 }
 
 function sacarImg() {
     fieldSet.innerHTML = `
         <legend>Imagenes</legend>
     `;
+
+    contImgs = 0;
+
 }
